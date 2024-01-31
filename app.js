3524602554348437163a6bdd9dcb39d835f51509
claudio peñalva <claudiosaldia777@gmail.com>
2024-01-31 09:17:28 -0300

    19

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// modulo nativo
+const path = require('node:path');
+// modulo 3ros
 const express = require('express');
 const app = express();
-const path = require('node:path');
+
 
 // Routes
 app.get('/', ( req, res ) =>{
@@ -12,13 +15,13 @@ app.get('/', ( req, res ) =>{
 
 
 
-// Public (static) Listen Server
+// Estatica la carpeta public
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*app.listen( 3030, () => console.log(`Server up on PORT:  http://localhost:3030`) )*/
 
-// esto lo cambie para usar rende
+// esto lo cambie para usar render
 const PORT = process.env.PORT || 3030
 
 app.listen(PORT, () => console.log(`Server up on PORT:  http://localhost:${PORT}`));
